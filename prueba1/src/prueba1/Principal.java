c77ad5447715b24f7a2b2acc6c457b880104bcd8
Tony <anthoska@hotmail.es>
2021-04-22 10:16:46 +0200

    Se notifica cambio1 en principal

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -3,5 +3,7 @@ package prueba1;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo. Java Git.");
+		System.out.println("Hola mundo. Java Git.");
+		System.out.println("Pruebas Git. Se añade cambio1.");
 	}
 }
